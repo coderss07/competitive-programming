a21efdbe10b9d74eb5e795df010935c15afbbd1c
sarthu858 <sarthu858@gmail.com>
2021-09-03 22:36:35 +0530

    Some Fixes

--- 1.cpp
+++ 1.cpp
@@ -1,6 +1,6 @@
 #include<bits/stdc++.h>
 #define ll long long int
-#define vvi vector<vector<ll> >
+#define vi vector< int >
 #define rep(i,a,b) for (int i = a; i < b; ++i)
 
 using namespace std;
@@ -10,13 +10,32 @@ using namespace std;
 int main() {
 	int t; cin>>t;
 	while(t--) {
-		int n, q; cin >> n >> q;
-		vvi adj(n + 1, vectot<int>(n + 1));
+		int n; cin >> n;
+		vi a(n + 1, 0);
+		bool flag = false;
+		int prev = 0;
+		rep(i, 1, n + 1) {
+			int data;
+			cin >> data;
+			if(data < prev) {
+				flag = true;
+			}
+			prev = data;
+			a[data]++;
+		}
+		if(flag) {
+			rep(i, 1, n + 1) {
+				if(a[i] == 0) {
+					flag = false;
+					break;
+				}
+			}
+		}
 		
-		if(L(s) == V(s)) {
-			cout << "YES" << endl;
+		if(flag) {
+			cout << "yes" << endl;
 		}else {
-			cout << "NO" << endl;
+			cout << "no" << endl;
 		}
 	}
 	return 0;
